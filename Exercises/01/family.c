55180385d09f56de23d7716e607d1e896f463a5a
Alex D <amdaley@udel.edu>
2023-03-24 22:46:29 -0400

    Exercises Folder with 01

---
+++ Exercises/01/family.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+    int status;
+    pid1 = fork();
+    if(pid1 == 0)
+	pid2 = fork()y
+ 
+    return 0;
+}
+
+
